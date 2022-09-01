dc615f6c59cb399617a4a28f1bc0fd36b33b1fcc
abhishekpotbhare01 <abhishek.potbhare01@gmail.com>
2022-09-01 10:36:42 +0530

    adding abhishek in logger

--- src/main/java/com/abhi/docker_jenkins_integration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/abhi/docker_jenkins_integration/DockerJenkinsIntegrationApplication.java
@@ -17,5 +17,6 @@ public class DockerJenkinsIntegrationApplication {
         logger.info("Application executed");
         logger.info("Application executed 2 line");
         SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+        logger.info("Hello ABhishek");
     }
 }
